9642c3c2caebce16a7dc45c5bc86cc215e28a5b1
John D <jp.delaney117@gmail.com>
2021-02-10 22:33:39 -0500

    Dig Assignment 1.0

---
+++ digassignment1/js/main.js
@@ -0,0 +1,80 @@
+import "./phaser.js";
+
+// You can copy-and-paste the code from any of the examples at https://examples.phaser.io here.
+// You will need to change the `parent` parameter passed to `new Phaser.Game()` from
+// `phaser-example` to `game`, which is the id of the HTML element where we
+// want the game to go.
+// The assets (and code) can be found at: https://github.com/photonstorm/phaser3-examples
+// You will need to change the paths you pass to `this.load.image()` or any other
+// loading functions to reflect where you are putting the assets.
+// All loading functions will typically all be found inside `preload()`.
+
+// The simplest class example: https://phaser.io/examples/v3/view/scenes/scene-from-es6-class
+
+class MyScene extends Phaser.Scene {
+    
+    constructor() {
+        super();
+        
+        this.bouncy = null;
+    }
+    
+    preload() {
+        game.stage.backgroundColor = '#85b5e1';
+
+        game.load.baseURL = 'http://examples.phaser.io/assets/';
+        game.load.crossOrigin = 'anonymous';
+
+        game.load.image('player', 'sprites/dude.png');
+        game.load.image('platform', 'sprites/platform.png');
+    }
+    
+    create() {
+        player = game.add.sprite(100, 200, 'player');
+
+        game.physics.arcade.enable(player);
+
+        player.body.collideWorldBounds = true;
+        player.body.gravity.y = 500;
+
+        platforms = game.add.physicsGroup();
+
+        platforms.create(500, 150, 'platform');
+        platforms.create(-200, 300, 'platform');
+        platforms.create(400, 450, 'platform');
+
+        platforms.setAll('body.immovable', true);
+
+        cursors = game.input.keyboard.createCursorKeys();
+        jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    }
+    
+    update() {
+        game.physics.arcade.collide(player, platforms);
+
+        player.body.velocity.x = 0;
+
+        if (cursors.left.isDown)
+        {
+            player.body.velocity.x = -250;
+        }
+        else if (cursors.right.isDown)
+        {
+            player.body.velocity.x = 250;
+        }
+
+        if (jumpButton.isDown && (player.body.onFloor() || player.body.touching.down))
+        {
+            player.body.velocity.y = -400;
+        }
+    }
+}
+
+const game = new Phaser.Game({
+    type: Phaser.AUTO,
+    parent: 'game',
+    width: 800,
+    height: 600,
+    scene: MyScene,
+    physics: { default: 'arcade' },
+    });
